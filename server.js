8437f4db6658ca092ce51a63e431d7cf9b611028
bharath <bharathhalemane143@gmail.com>
2025-06-28 22:29:49 +0530

    updated server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.get("/homepage", (req, res) => {
+    res.render("homepage")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
 })
 
 app.listen(3000)
